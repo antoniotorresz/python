25a35ace55ce018b0b9e4707a9dd48d5a45238ca
debian <debian@debian.ipv6.infinitum.net.mx>
2024-02-21 18:01:19 -0600

    added files

--- invest/investment.py
+++ invest/investment.py
@@ -1,12 +1,12 @@
 def calculate_investment():
     yield_rate = 12.5 # 12.5% annual return
     current_investment = 8137 # initial investment
-    weeks = 26 # 0.5 year
+    weeks = 28 # 0.5 year
     subscription = 2500 # weekly subscription
-    for week in range(weeks):
+    for week in range(1, weeks):
         week_yield = current_investment * (yield_rate / 100) / 52
         current_investment = current_investment + subscription + week_yield
         if week % 2 == 0:
-            print(f'Week {week + 1}, with yield of: ${week_yield:.2f} will save ${current_investment:.2f}')
+            print(f'Week {week}, with yield of: ${week_yield:.2f} will save ${current_investment:.2f}')
 
 calculate_investment()
